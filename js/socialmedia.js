0b00a23dc7ccb6da8b4addbb2b0da4ae1cd73b42
Nicolas Herrera <nicolas.herrera@autodesk.com>
2014-04-17 14:46:33 -0400

    Broad updates to the sharing scripts

--- js/socialmedia.js
+++ js/socialmedia.js
@@ -2,88 +2,49 @@
 
 window.socialmedia = window.socialmedia || {};
 
-(socialmedia.ui = function($){
+(window.socialmedia = function($){
 
     'use strict';
 
     //private members
-    var _ns      = socialmedia.ui || {},
-        _services = ['fb', 'tw', 'li'];
+    var _ns       = socialmedia || {},
+        _services = [];
 
     //app initialization module
     (function(){
 
-        //expose app init
-        _ns.init = function(options){
+        _ns.registerService = function(options){
 
-            console.log(_services);
+            _services.push(options.id);
 
-            var i = 0,
-                l = _services.length,
-                target,
-                url = 'http://calamitoso.github.io/socialmedia/',
-                encodedUrl = encodeURIComponent(url),
-                statsUrl,
-                ajax = {
-                    'collection' : [],
-                    'fn': {
-                        'donefb' : function( data, textStatus, jqXHR ){
-                            console.log('+++ done fb', data, textStatus, jqXHR );
-                            $('#fb').find('.social-media-count').html(data[0].share_count);
-                        },
-                        'donetw' : function( data, textStatus, jqXHR ){
-                            // console.log('+++ done tw', data, textStatus, jqXHR );
-                            $('#tw').find('.social-media-count').html(data.count);
-                        },
-                        'doneli' : function( data, textStatus, jqXHR ){
-                            // console.log('+++ done li', data, textStatus, jqXHR );
-                            $('#li').find('.social-media-count').html(data.count);
-                        },
-                        'fail' : function(){
-                            console.log('--- fail', this);
-                        },
-                        'always' : function(){
-                        }
-                    }
-                };
+            //populate stats
+            $.ajax( options.statsUrl, { dataType: 'jsonp' } )
+                .done(options.statsCallback)
+                .fail( function(a, b, c) {
+                    console.log(a, b, c);
+                });
 
-            for(;i < l; i++ ){
+            //prepare target
+            $('#' + options.id)
+                .attr('href', options.shareUrl)
+                .on('click', function(e){
+                    console.log($(this).attr('href'));
 
-                target = $('#' + _services[i]);
+                    e.preventDefault();
+                    var winHeight = 350;
+                    var winWidth = 520;
+                    var winTop = (screen.height / 2) - (winHeight / 2);
+                    var winLeft = (screen.width / 2) - (winWidth / 2);
+                    window.open($(this).attr('href'), 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width='+winWidth+',height='+winHeight);
 
-                switch(_services[i]){
-                    case 'fb':
-                        target.attr('href', 'https://www.facebook.com/sharer/sharer.php?u='+ encodedUrl);
-                        statsUrl = 'https://api.facebook.com/method/links.getStats?urls=' + encodedUrl + '&format=json';
-                        break;
+                    
+                });
 
-                    case 'tw':
-                        target.attr('href', 'https://twitter.com/intent/tweet?url='+ encodedUrl);
-                        statsUrl = 'http://urls.api.twitter.com/1/urls/count.json?url=' + encodedUrl;
-                        break;
-
-                    case 'li':
-                        target.attr('href', 'http://www.linkedin.com/shareArticle?mini=true&amp;url='+ encodedUrl);
-                        statsUrl = 'https://www.linkedin.com/countserv/count/share?url=' + encodedUrl;
-
-                        break;
-                }
-
-                console.log(statsUrl);
-
-                ajax.collection.push(
-                    $.ajax( statsUrl, { dataType: 'jsonp' } )
-                        .done(ajax.fn['done' + _services[i]])
-                        .fail(ajax.fn.fail)
-                        .always(ajax.fn.always)
-                );
-            }
-
-            // window.myAjax = ajax;
-
-            //unregister init function after first use
-            delete socialmedia.ui.init;
         };
+
+        // _ns.getServices = function(){
+        //     return _services;
+        // };
 
     }());
 
@@ -94,6 +55,63 @@ window.socialmedia = window.socialmedia || {};
 
 $(function() {
     'use strict';
-    socialmedia.ui.init({});
-});
+    //socialmedia.init({});
 
+    var url         = 'http://calamitoso.github.io/socialmedia/',
+        encodedUrl  = encodeURIComponent(url);
+
+    socialmedia.registerService({
+        id: 'fb',
+        statsUrl: 'https://api.facebook.com/method/links.getStats?urls=' + encodedUrl + '&format=json',
+        statsCallback: function(data){
+            $('#fb-count').html(data[0].share_count);
+        },
+        shareUrl: 'https://www.facebook.com/sharer/sharer.php?u='+ encodedUrl
+    });
+
+    socialmedia.registerService({
+        id: 'tw',
+        statsUrl: 'http://urls.api.twitter.com/1/urls/count.json?url=' + encodedUrl,
+        statsCallback: function(data){
+            $('#tw-count').html(data.count);
+        },
+        shareUrl: 'https://twitter.com/intent/tweet?url='+ encodedUrl
+    });
+
+    socialmedia.registerService({
+        id: 'li',
+        statsUrl: 'https://www.linkedin.com/countserv/count/share?url=' + encodedUrl,
+        statsCallback: function(data){
+            $('#li-count').html(data.count);
+        },
+        shareUrl: 'http://www.linkedin.com/shareArticle?mini=true&url='+ encodedUrl
+    });
+
+    $('.panel-share').on('click', function(e){
+        e.preventDefault();
+
+        var panel = $(this).closest('.panel'),
+            name    = panel.find('.panel-name'),
+            picture = panel.find('img'),
+            fBUIOptions = {
+                method:     'feed',
+                link:       'http://calamitoso.github.io/socialmedia/',
+                name:       name.html(),
+                picture:    window.location.host + '/' + picture.attr('src')
+            };
+
+        console.log(fBUIOptions);
+
+
+        FB.ui(fBUIOptions, function(response){
+            console.log(response);
+        });
+    });
+
+    $.getScript('//connect.facebook.net/en_UK/all.js', function(){
+        FB.init({
+            appId: '224831637710153',
+        });
+    });
+
+});
